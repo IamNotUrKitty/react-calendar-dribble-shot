460777fcf474061b23e00964af49d75acbe3f692
lirla@gmail.com <firexable@gmail.com>
2016-02-18 22:17:08 +0300

    Add Array.fill polyfill cause IE

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import RangePicker from './components/main'
 require('../styles/style.styl');
+require('./utils/array.fill.js');
 
 ReactDom.render(<RangePicker nameFrom="dateFrom" nameTo="dateTo"/>, document.querySelector('#app'));
